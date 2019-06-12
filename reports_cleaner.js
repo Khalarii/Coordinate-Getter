d064faa090089139fca5a8e45fdfb74ac03aa317
Kimgss <kimgss94@hotmail.com>
2019-06-12 00:35:21 -0300

    Beautiful

--- reports_cleaner.js
+++ reports_cleaner.js
@@ -7,13 +7,15 @@ if (current_url.includes('tribalwars') && current_url.includes('village=') && cu
         for (var i = 1; i < reports_table.length; i++) {
         	//var village_info = reports_table[i - village_info_offset].cells[0].childNodes[1].childNodes[1].childNodes[1].innerText;
             var row = reports_table[i];
-            var trigger_words = { 'forneceu', 'oferta', 'apoiou' };
-            trigger_words.forEach(function(word){ if (row.cells[1].firstChild.data.includes('forneceu')){
-            	var l = row.cells[0].firstChild.data;
-                alert(l);
-                break;
-            } else{ alert(row.cells[1].firstChild.data); break; } });
+            var trigger_words = [ 'forneceu', 'oferta', 'apoiou', 'GC' ];
+            for (var j = 0; j < trigger_words.length; j++){
+                if (row.innerText.includes(trigger_words[j])){
+                    row.cells[0].firstElementChild.checked = true;
+                }
+            }
         }
+
+        document.getElementsByClassName('btn btn-cancel')[0].click();
     } else {
         alert('Erro ao encontrar tabela de relatorios');
     }
